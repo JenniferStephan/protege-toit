d35d5fbc8f1d216c40555e71edab12f84abbfee4
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-03-31 15:47:45 +0200

    clean

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
