6437af2fdeb0c92ff798ac94f4da2871539a8c1a
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-03-31 16:19:06 +0200

    crud hebergement ok

--- app/controllers/hebergements_controller.rb
+++ app/controllers/hebergements_controller.rb
@@ -11,4 +11,28 @@ class HebergementsController < ApplicationController
     end
   end
 
+  def show
+    @hebergement = Hebergement.find(params[:id])
+  end
+
+  def new
+    @hebergement = Hebergement.new
+  end
+
+  def create
+    @hebergement = Hebergement.new(heb_params)
+    @hebergement.user = current_user
+    if @hebergement.save
+      redirect_to hebergement_path(@hebergement)
+    else
+      render :new
+    end
+  end
+
+private
+
+def heb_params
+    params.require(:hebergement).permit(:name, :address, :nbmax)
+end
+
 end
